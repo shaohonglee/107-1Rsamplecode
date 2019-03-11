2e7fb0d3b90b71d6e7538b4f3e443088e81aa49f
shaohonglee <d38372002@gmail.com>
2019-03-11 16:28:56 +0800

    Version2

--- hw_1/hw_1.R
+++ hw_1/hw_1.R
@@ -1,6 +1,8 @@
 setwd("~/GitHub/Politics-and-information")
 library(dplyr)
 library(tm)
+
+
 data_201801=data.table::fread('data/201801_data.csv',data.table = F,encoding = 'UTF-8')
 data_201802=data.table::fread('data/201802_data.csv',data.table = F,encoding = 'UTF-8')
 data_201803=data.table::fread('data/201803_data.csv',data.table = F,encoding = 'UTF-8')
@@ -19,12 +21,13 @@ top10=merge(page_count,data_201801,by="Page_Name")
 top10=top10[,-2]
 
 mean(nchar(top10$Message))
-filter(top10,grepl("柯文哲",top10$Message)==T)%>%count() #355
+KP=filter(top10,grepl("柯文哲",top10$Message)==T)
 filter(top10,grepl("姚文智",top10$Message)==T)%>%count() #31
 filter(top10,grepl("丁守中",top10$Message)==T)%>%count() #77
-
-filter(top10,grepl("韓國瑜",top10$Message)==T)%>%count() #27
-filter(top10,grepl("陳其邁",top10$Message)==T)%>%count() #40
+#####################
+han=filter(alldata,grepl("韓國瑜",alldata$Page_Name)==T&grepl("韓國瑜粉絲團",alldata$Page_Name)==F&grepl("韓國瑜新聞網",alldata$Page_Name)==F&grepl("高雄選韓國瑜News",alldata$Page_Name)==F&grepl("侯友宜 盧秀燕 韓國瑜 北中南連線",alldata$Page_Name)==F&grepl("高雄在地韓國瑜News",alldata$Page_Name)==F) #27
+#####################
+chen=filter(alldata,grepl("陳其邁",alldata$Page_Name)==T&grepl("陳其邁的潛水日記",alldata$Page_Name)==F)
 
 filter(top10,grepl("#",top10$Message)==T)%>%count()      #575  有TAG的
 filter(top10,grepl("https",top10$Message)==T)%>%count()  #291  有連結的
@@ -41,7 +44,14 @@ top10$id=paste(top10$Page_ID,top10$created_time,sep = "@") # 合併
 
 #########################################################################
 #top10$Message=top10$Message%>%removePunctuation()%>%removeNumbers()
-    
-alldata$Date=as.POSIXct(alldata$Date,format="%Y/%m/%d %H:%M:%S")
-before=filter(alldata,Date<"2018-10-01")
-after=filter(alldata,Date>"2018-09-30")   
+#############################################################    
+han$Date=as.POSIXct(han$Date,format="%Y/%m/%d %H:%M:%S")
+chen$Date=as.POSIXct(chen$Date,format="%Y/%m/%d %H:%M:%S")
+plot(han$Date,han$Comment_Count,type = "l")
+lines(chen$Date,chen$Comment_Count,type = "l",col="red")
+lines(chen$Date,chen$LIKE_COUNT,type = "l",col="blue")
+lines(han$Date,han$LIKE_COUNT,type = "l",col="yellow")
+###########################################################
+before=filter(alldata,Date<"2018-10-01"&Date>"2018-07-01")
+after=filter(alldata,Date>"2018-10-01"&Date<"2019-01-01")
+before
